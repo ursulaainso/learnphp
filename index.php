9c62c36fd41cb14dad79556429552fb2fa5d8676
Ursula Ainso <ursulaainso@gmail.com>
2024-10-26 11:14:34 +0300

    Text functions

--- index.php
+++ index.php
@@ -1,20 +1,9 @@
 <?php
 
-$num = 1;
-$num = 2.3;
-$string = 'Hello World';
-$bool = true;
-$bool = false;
-$var;
-$array = ['cherry', 'mango', null, false, 13];
-$object = new stdClass();
-
-
-$num += 4;
-$num -=4;
-$num %= 3;
-$num = 5 % 2; //modulu, jäägiga jagamine, jääk 1
-$num = cos(1.2);
-$num = sqrt(9);
+$text = 'hello' . 'world';
+$text .= 'asaaa';
+$text = str_repeat('dsdssa');
+$text = strtoupper('hello');
+$text ++;
 
 var_dump($array);
