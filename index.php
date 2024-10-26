3dca7288a4d4a072e23b7ac911b8cffaea443b14
Ursula Ainso <ursulaainso@gmail.com>
2024-10-26 11:54:28 +0300

    Boolean

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
 
-$text = 'hello' . 'world';
-$text .= 'asaaa';
-$text = str_repeat('dsdssa');
-$text = strtoupper('hello');
-$text ++;
+$bool = true && true; //mõlemad peavad olema tõesed
+$bool = true;
+$bool = !false; //ümberpööramise tehe
+$bool = true xor false; //tõene ainult siis kui mõlemad osapooled on erinevad
 
-var_dump($array);
+var_dump($bool);
