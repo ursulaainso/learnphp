cbe2ebc11d128c011bb90ed53b8de803c7a6742b
Ursula Ainso <ursulaainso@gmail.com>
2024-10-26 10:45:11 +0300

    Add types

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 
-echo 'Hello PHP';
+$num = 1;
+$num = 2.3;
+$string = 'Hello World';
+$bool = true;
+$bool = false;
+$var;
+$array = ['cherry', 'mango', null, false, 13];
+$object = new stdClass();
 
-?>
+var_dump($array);
