31ff556f75d0eb0aebea71ed738352ed14a5eef3
Ursula Ainso <ursulaainso@gmail.com>
2024-10-26 11:05:24 +0300

    Number functions

--- index.php
+++ index.php
@@ -9,4 +9,12 @@ $var;
 $array = ['cherry', 'mango', null, false, 13];
 $object = new stdClass();
 
+
+$num += 4;
+$num -=4;
+$num %= 3;
+$num = 5 % 2; //modulu, jäägiga jagamine, jääk 1
+$num = cos(1.2);
+$num = sqrt(9);
+
 var_dump($array);
